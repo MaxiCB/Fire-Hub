b705cb68f7851666e2877c8c2fadd71235c86d11
Aaron Beasley <aaroncb@aaroncb.com>
2019-12-25 23:48:24 -0600

    Implemented firebase credentials, firebase flow. Looking for bug preventing signup and login.

--- fire-hub/src/components/Firebase/firebase.js
+++ fire-hub/src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const prodConfig = {
     apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -23,6 +24,28 @@ const prodConfig = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth;
   }
+
+//   Auth Implementation
+
+doCreateUserWithEmailAndPassword = (email, password) => {
+    console.log('here')
+    this.auth.createUserWithEmailAndPassword(email, password);
+}
+    
+  doSignInWithEmailAndPassword = (email, password) => {
+    console.log('here2')
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+
 }
 export default Firebase;
